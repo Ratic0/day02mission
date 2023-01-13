cc5935a01fd5045ff449a66d9221314880f5973d
KimTaeYeong <12170548@inha.edu>
2023-01-13 16:17:54 +0900

    day02_mission02

--- day02_mission.py
+++ day02_mission.py
@@ -1,11 +1,24 @@
 import random
 
-secret = random.randint(1,10) #random 모듈에서 불러와 1~10 사이의 정수가 임의로 발생
-guess = random.randint(1,10)
-print(f'secret={secret}  guess={guess}')
-if secret - guess > 0 :
-    print('too low')
-elif secret == guess :
-    print('just right')
-else :
-    print('too high')
+small_num = random.randint(0,1) #random 모듈에서 불러와 0~1 사이의 정수가 임의로 발생
+green_num = random.randint(0,1)
+
+small = bool(small_num) #1이 TRUE
+green = bool(green_num)
+
+print(f'small : {small} | green : {green}')
+
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+O  = '\033[33m' # orange
+
+if small == True and green == True :
+    print(G+'이건 완두콩이야!'+W)
+elif small == True and green == False :
+    print(R+'이건 체리야!'+W)
+elif small == False and green == True :
+    print(G+'이건 수박이야!'+W)
+elif small == False and green == False :
+    print(O+'이건 호박이야!'+W)
+
